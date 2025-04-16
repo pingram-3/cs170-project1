a1cf9a7072ec85b4c4dae707b00877a1cab0a58d
Nathan Ha <nathanha562@gmail.com>
2025-04-16 14:37:47 -0700

    added more user i/o code

--- app/main.py
+++ app/main.py
@@ -2,29 +2,65 @@ from .tree import Node, Tree
 
 
 def main():
+    n = 3
+    puzzle_grid = [
+        [1, 2, 3],
+        [4, 8, 0],
+        [7, 6, 5],
+    ]
+
     # Prompt user input
-    print("Enter a puzzle. The puzzle is an NxN grid of numbers.")
     print(
-        "Separate numbers in a row with space, and separate rows by pressing enter."
+        'Welcome to nha023 and ____ 8 puzzle solver. Type “1” to use a default puzzle, or “2” to enter your own puzzle.'
     )
-    user_input = input().split()
+    user_choice = int(input())
 
-    # build puzzle grid from input
-    temp_row = [num for num in user_input]
-    n = len(temp_row)
-    puzzle_grid = [temp_row]
-
-    # gets the rest of the puzzle grid based on the size of the first row
-    for _ in range(n - 1):
-        user_input = input().split()
-        temp_row = [num for num in user_input]
-        puzzle_grid.append(temp_row)
+    if (user_choice == 1):
+        pass  # logic to solve default puzzle goes here
+    elif (user_choice == 2):
+        print('Enter your puzzle, use a zero to represent the blank')
+        puzzle_grid = []
+        # builds puzzle grid from input
+        for i in range(n):
+            user_input = input(
+                f'Enter row {i + 1}, use space or tabs between numbers '
+            ).split()
+            if (len(user_input) != n):
+                print(
+                    f'Invalid row size of {len(user_input)}, expected: {n}. Terminating program.'
+                )
+                return
+            temp_row = [int(num) for num in user_input]
+            puzzle_grid.append(temp_row)
+    else:
+        print('Invalid choice, terminating program')
+        return
 
     # outputs the stored puzzle for testing purposes
-    print("Your puzzle is:")
+    print('Your puzzle is:')
     for row in puzzle_grid:
-        print(" ".join(row))
+        print(row)
+
+    print('Enter the number for your choice of algorithm:')
+    print('(1) Uniform Cost Search')
+    print('(2) A* with the Misplaced Tile heuristic.')
+    print('(3) A* with the Euclidean distance heuristic.')
+    user_choice = int(input())
+
+    # logic to solve puzzle goes here
+    if (user_choice == 1):
+        # Uniform Cost Search code goes here
+        pass
+    elif (user_choice == 2):
+        # A* with the Misplaced Tile heuristic code goes here
+        pass
+    elif (user_choice == 3):
+        # A* with the Euclidean distance heuristic code goes here
+        pass
+    else:
+        print('Invalid choice, terminating program')
+        return
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
