7b3c15c57a6e845d2e48b394b66172505c4c6152
Nathan Ha <nathanha562@gmail.com>
2025-04-16 12:44:11 -0700

    Made basic user input

--- app/main.py
+++ app/main.py
@@ -2,7 +2,27 @@ from .tree import Node, Tree
 
 
 def main():
-    print("Hello from the main file")
+    # Prompt user input
+    print("Enter a puzzle. The puzzle is an NxN grid of numbers.")
+    print("Separate numbers in a row with space, and separate rows by pressing enter.")
+    user_input = input().split()
+    
+    # build puzzle grid from input
+    temp_row = [num for num in user_input]
+    n = len(temp_row)
+    puzzle_grid = [temp_row]
+    
+    # gets the rest of the puzzle grid based on the size of the first row
+    for _ in range(n - 1):
+        user_input = input().split()
+        temp_row = [num for num in user_input]
+        puzzle_grid.append(temp_row)
+
+    # outputs the stored puzzle for testing purposes
+    print("Your puzzle is:")
+    for row in puzzle_grid:
+        print(" ".join(row))
+
 
 
 if __name__ == "__main__":
